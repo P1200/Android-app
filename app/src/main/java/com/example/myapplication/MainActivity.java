42a011795d5c48e0311deef346480b7bad282f0b
Mateusz Wołek <sekwoja5@gmail.com>
2022-10-15 10:51:51 +0200

    Add example menu proposition and two layouts in different phone positions (vertical and horizontal)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        if (getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_PORTRAIT)
+        {
+            fragmentTransaction.replace(R.id.linlay, new HorizontalLayoutFragment());
+        }
+        else
+        {
+            fragmentTransaction.replace(R.id.linlay, new VerticalLayoutFragment());
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
