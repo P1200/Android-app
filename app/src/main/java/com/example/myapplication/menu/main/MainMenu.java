d6e7f42887e0567ec317951404db11dbb7ee2678
Mateusz Wołek <sekwoja5@gmail.com>
2022-12-22 10:03:27 +0100

    Add google map with current user localization.

--- app/src/main/java/com/example/myapplication/menu/main/MainMenu.java
+++ app/src/main/java/com/example/myapplication/menu/main/MainMenu.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,5 +30,11 @@ public class MainMenu extends AppCompatActivity {
             fragmentTransaction.replace(R.id.linlay, new HorizontalMenu());
         }
         fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        Toast.makeText(getApplicationContext(), "Witamy w aplikacji NoteExtended", Toast.LENGTH_LONG).show();
+        super.onStart();
     }
 }
